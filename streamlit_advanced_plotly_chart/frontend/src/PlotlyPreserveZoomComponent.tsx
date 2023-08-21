48945ba41c0854c060136a44bed6173c4d7763db
kaiser-data <61822885+kaiser-data@users.noreply.github.com>
2023-08-21 23:23:41 +0200

    Update PlotlyPreserveZoomComponent.tsx

--- streamlit_advanced_plotly_chart/frontend/src/PlotlyPreserveZoomComponent.tsx
+++ streamlit_advanced_plotly_chart/frontend/src/PlotlyPreserveZoomComponent.tsx
@@ -35,10 +35,12 @@ const PlotlyPreserveZoomComponent = (props: ComponentProps): any => {
   const plotlyEventHandler = (eventData: any) => {
     // If no event data, send only the current range to Streamlit
     if (eventData === null) {
-      const range = state.layout.xaxis.range;
+      const range_x = state.layout.xaxis.range;
+      const range_y = state.layout.yaxis.range;
       const clickedPointsDict = {
         points: [],
-        selected_range: range
+        selected_range_x: range_x,
+        selected_range_y: range_y
       }
       Streamlit.setComponentValue(clickedPointsDict);
       return;
